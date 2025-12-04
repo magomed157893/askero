dfa1d0cc2ea4a2c4d0c7bfae712ea3edaaf11400
Magomed Aliev <magomed157893@gmail.com>
2025-12-04 13:31:22 +0300

    Uploaded a laravel project

--- public/index.php
+++ public/index.php
@@ -1,12 +1,20 @@
 <?php
 
-header('Content-Type: application/json; charset=utf-8');
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-$data = [
-    'id' => 12345,
-    'name' => 'Aliev Magomeddibir Zamirovich',
-    'email' => 'magomed157893@gmail.com',
-    'age' => 24
-];
+define('LARAVEL_START', microtime(true));
 
-echo json_encode($data);
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
